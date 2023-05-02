28e7038bc1da5c4e1266d6c8f503b312cad046ae
brunalmucelli <brunalmucelli@gmail.com>
2023-05-02 05:02:03 -0300

    Refatoração CLI - recebendo diretórios no terminal

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function extraiLinks(texto) {
   const resultados = capturas.map(captura => ({
     [captura[1]]: captura[2]
   }));
-  return resultados;
+  return resultados.length !== 0 ? resultados : "Não há links no arquivo";
 };
 
 function trataErro(erro) {
@@ -19,10 +19,10 @@ async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro);
-  } 
+  }
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
